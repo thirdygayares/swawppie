466c5345d2d68088884b770c958f9a18bf7e5d21
thirdy454 <gayaresthird@gmail.com>
2022-03-21 16:12:39 +0800

    nilipat sa stateless

--- lib/main.dart
+++ lib/main.dart
@@ -5,43 +5,48 @@ import 'package:flutter/material.dart';
 //import 'widget.dart';
 
 void main() =>
-  //runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Practice()));
+    //runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Practice()));
 
-  //start of practice from the scratch
+    //start of practice from the scratch
 
-  runApp(MaterialApp(
-    //scaffold widget ? navbar
-    home: Scaffold(
+    runApp(MaterialApp(
+      //scaffold widget ? navbar
+      home: test(),
+    ));
+
+//REMEMBER STATELESS - cannot change every time
+//STATEFUL - change
+
+class test extends StatelessWidget {
+  const test({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       //appbar
       appBar: AppBar(
-      title: Text('my First App'),
-      centerTitle: true,
-      backgroundColor: Color.fromARGB(255, 244, 67, 54),
-      ), 
-    body: Center( 
-      child: Text(
-        'hello Thirdy',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'OpenSans',
-        ),
+        title: Text('my First App'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 244, 67, 54),
+      ),
+      body: Center(
+        child: Text(
+          'hello Thirdy',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'OpenSans',
+          ),
         ),
       ),
 
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('click'),
-      backgroundColor: Color.fromARGB(255, 244, 67, 54),
-    ),
-
-    ),
-
-  
-
-
-  
-  ));
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Color.fromARGB(255, 244, 67, 54),
+      ),
+    );
+  }
+}
