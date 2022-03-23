e10d1def508e15cdc6012e00c9192121b861cd78
thirdy454 <gayaresthird@gmail.com>
2022-03-23 18:13:13 +0800

    lIST OF DATA

--- lib/main.dart
+++ lib/main.dart
@@ -12,112 +12,24 @@ class ThirdyCard extends StatefulWidget {
 }
 
 class _ThirdyCardState extends State<ThirdyCard> {
-  int thirdy = 0;
+  List<String> quotes = [
+    'Si thirdy ay pogi',
+    'Si thirdy ay matalino',
+    'si thirdy ay cute',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 248, 61, 61),
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('ThirdyGraphicsStudio'),
-        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
+        title: Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            thirdy += 1;
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[800],
+      body: Column(
+        children: quotes.map((quotes) => Text(quotes)).toList(),
       ),
-      body: Padding(
-          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: CircleAvatar(
-                  // backgroundImage: AssetImage('images/profile/jose.png'),
-                  radius: 40.0,
-                ),
-              ),
-              Divider(
-                height: 60.0,
-                color: Colors.grey[800],
-              ),
-              Text(
-                'Name',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              // adding space
-              SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                'Thirdy',
-                style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-              SizedBox(
-                height: 30.0,
-              ),
-
-              Text(
-                'Location',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              // adding space
-              SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                '$thirdy',
-                style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-              SizedBox(
-                height: 30.0,
-              ),
-
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color: Colors.grey[400],
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'gayaresthirdy@gmail.com',
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 18.0,
-                      letterSpacing: 1.0,
-                    ),
-                  )
-                ],
-              ),
-            ],
-          )),
     );
   }
 }
